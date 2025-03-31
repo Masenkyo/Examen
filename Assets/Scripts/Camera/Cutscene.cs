0fcdfef22d295f7a09e185ce077d94cb5ca3c2cf
Masenkyo <33390@ma-web.nl>
2025-03-31 09:47:42 +0200

    cutscene push

---
+++ Assets/Scripts/Camera/Cutscene.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using UnityEngine;
+
+public class Cutscene : MonoBehaviour
+{
+
+
+
+
+
+    void CutsceneOfLevel()
+    {
+        
+    }
+
+    IEnumerator StartAnimation()
+    {
+        
+        
+    }
+    
+}
