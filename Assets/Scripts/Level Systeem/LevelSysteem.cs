e0df5868a58cbe8b98285c0e1c978f7badc0c403
Masenkyo <33390@ma-web.nl>
2025-04-01 10:31:58 +0200

    Added comments to the scripts + removed random square in the main game scene + fixed a error that the reference in levelsystem was not there

--- Assets/Scripts/Level Systeem/LevelSysteem.cs
+++ Assets/Scripts/Level Systeem/LevelSysteem.cs
@@ -6,13 +6,21 @@ using UnityEngine.Events;
 
 public class LevelSysteem : MonoBehaviour
 {
+    // Level settings and list
     [SerializeField] List<Transform> Levels = new();
     public int amountOfLevels = 10;
     public float gapBetweenLevels = -8.5f;
     public UnityEvent Ready;
+    
+    // A reference to this script
+    public static LevelSysteem instance;
 
     void Start() => InstantiateLevels();
     
+    // The reference
+    void Awake() => instance = this;
+
+    // Instantiate random levels on specified places
     void InstantiateLevels()
     {
         for (float i = 0; i > amountOfLevels * gapBetweenLevels; i += gapBetweenLevels) 
