fa9dce13cd7d50e6aef3b2ee7329d9a05922d7c4
Masenkyo <33390@ma-web.nl>
2025-04-16 13:47:20 +0200

    removed random using's

--- Assets/Scripts/Flippers/Flipper.cs
+++ Assets/Scripts/Flippers/Flipper.cs
@@ -1,11 +1,7 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Events;
 using UnityEngine.InputSystem;
-using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class Flipper : MonoBehaviour
