b63bb700db42a4420e00a845ed62f24c8a40d38b
Teo <90894954+TeoPara@users.noreply.github.com>
2025-05-14 11:33:39 +0200

    Revert "fix missclick"
    This reverts commit 6fa179aa125d1525dfa3a0f564a8fbe24b7bf24d.

--- Assets/Scripts/Flippers/MovingFlipper.cs
+++ Assets/Scripts/Flippers/MovingFlipper.cs
@@ -1,9 +1,9 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
-using UnityEngine.InputSystem;
-
 public class MovingFlipper : Flipper
 {
-    [SerializeField] Transform point1, point2;
+    [SerializeField] Transform point1, point2, point3;
     [SerializeField] int movementSpeed;
     float distance;
     public LineRenderer lr;
@@ -15,12 +15,15 @@ public class MovingFlipper : Flipper
         var a = GetComponent<LineRenderer>();
 		a.SetPosition(0, point1.position);
 		a.SetPosition(1, point2.position);
+		a.SetPosition(2, (point2.position + point1.position) / 2);
+		a.SetPosition(3, point3.position);
         lr = a;
+        rigidbody.position = point1.position;
     }
-    
-    override protected void Update()
+
+    protected override void FixedUpdate()
     {
-        base.Update();
+        base.FixedUpdate();
         MoveFlipper();
     }
 
@@ -29,45 +32,55 @@ public class MovingFlipper : Flipper
     
     void MoveFlipper()
     {
-        var DirTo1 = (point1.position - (Vector3)rigidbody.position).normalized; 
-        var DirTo2 = (point2.position - (Vector3)rigidbody.position).normalized;
-
-        // going to 1
-
-
-        if (brokenFlipper)
-            return;
-        
-        if (AltInputKeyboard is { } b)
-        {
-            if (Time.deltaTime * movementSpeed * 2 is { } step &&
-                step > Vector3.Distance(rigidbody.position, (b ? point1 : point2).position))
-                rigidbody.position +=
-                    (Vector2)(((b ? point1 : point2).position - (Vector3)rigidbody.position).normalized * step);
-            else
-                rigidbody.position = (b ? point1 : point2).position;
-            return;
-        }
-        
+	    if (brokenFlipper)
+		    return;
         if (InputJoystickMovement == Vector3.zero)
             return;
         
-        if (Vector3.Distance(InputJoystickMovement, DirTo1) < Vector3.Distance(InputJoystickMovement, DirTo2))
+        var DirTo1 = (point1.position - (Vector3)rigidbody.position).normalized; 
+        var DirTo2 = (point2.position - (Vector3)rigidbody.position).normalized;
+
+        var left = point1.position;
+        var right = point2.position;
+        var bottom = point3.position;
+        var mid = (left + right) / 2;
+       
+        bool leftright = Vector3.Distance((left - (Vector3)rigidbody.position).normalized, (left - right).normalized) < 0.05f;
+        bool middown = Vector3.Distance((bottom - (Vector3)rigidbody.position).normalized, (bottom - mid).normalized) < 0.05f;
+        if (Vector3.Distance(rigidbody.position, mid) < 0.1f || Vector3.Distance(rigidbody.position, bottom) < 0.1f)
+            middown = true;
+        if (Vector3.Distance(rigidbody.position, left) < 0.1f || Vector3.Distance(rigidbody.position, right) < 0.1f)
+            leftright = true;
+        
+        List<Vector3> options = new();
+        if (leftright)
+            options.AddRange(new[] { left, right });
+        if (middown)
+            options.AddRange(new[] { mid, bottom });
+
+        if (options.Count == 0)
+            return;
+        
+        // if (AltInputKeyboard is { } b)
+        // {
+        //    if (Time.deltaTime * movementSpeed * 2 is { } step &&
+        //        step > Vector3.Distance(rigidbody.position, (b ? chosenPoint1 : chosenPoint2)))
+        //        rigidbody.position += ((b ? chosenPoint1 : chosenPoint2) - rigidbody.position).normalized * step;
+        //    else
+        //        rigidbody.position = b ? chosenPoint1 : chosenPoint2;
+        //    return;
+        // }
+//
+        var closest = options.OrderBy(_ => Vector3.Distance(InputJoystickMovement, (_ - (Vector3)rigidbody.position).normalized )).First();
+
+
+        if (Time.deltaTime * movementSpeed * InputJoystickMovement.magnitude is { } step
+            && Vector3.Distance(rigidbody.position, closest) > step)
         {
-            if (Time.deltaTime * movementSpeed * (1 - Vector3.Distance(InputJoystickMovement, DirTo1) + 1) is { } step 
-                && Vector3.Distance(rigidbody.position, point1.position) > step)
-                rigidbody.position += (Vector2)((point1.position - (Vector3)rigidbody.position).normalized * step);
-            else
-                rigidbody.position = point1.position;
+            rigidbody.position += (Vector2)(closest - (Vector3)rigidbody.position).normalized * step;
         }
-        else // going to 2
-        {
-            if (Time.deltaTime * movementSpeed * (1 - Vector3.Distance(InputJoystickMovement, DirTo2) + 1) is { } step 
-            && Vector3.Distance(rigidbody.position, point2.position) > step)
-                rigidbody.position += (Vector2)((point2.position - (Vector3)rigidbody.position).normalized * step);
-            else 
-                rigidbody.position = point2.position;
-        }
+        else
+           rigidbody.position = closest;
       
     }
 }
