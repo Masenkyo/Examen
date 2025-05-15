6da2e132bf44132d7f6321d92481a7937d60ce40
Teo <90894954+TeoPara@users.noreply.github.com>
2025-05-15 10:22:02 +0200

    L Shape. Choose between 2 or 4 points

--- Assets/Scripts/Flippers/MovingFlipper.cs
+++ Assets/Scripts/Flippers/MovingFlipper.cs
@@ -3,7 +3,7 @@ using System.Linq;
 using UnityEngine;
 public class MovingFlipper : Flipper
 {
-    [SerializeField] Transform point1, point2, point3;
+    [SerializeField] Transform point1, point2, point3, point4;
     [SerializeField] int movementSpeed;
     float distance;
     public LineRenderer lr;
@@ -15,10 +15,19 @@ public class MovingFlipper : Flipper
         var a = GetComponent<LineRenderer>();
 		a.SetPosition(0, point1.position);
 		a.SetPosition(1, point2.position);
-		a.SetPosition(2, (point2.position + point1.position) / 2);
-		a.SetPosition(3, point3.position);
+		a.SetPosition(2, point3 != null ? point3.position : point1.position);
+		a.SetPosition(3, point4 != null ? point4.position : point2.position);
         lr = a;
         rigidbody.position = point1.position;
+
+        if (a.GetPosition(1) == a.GetPosition(2))
+        {
+            Vector3[] posses = new Vector3[a.positionCount];
+            a.GetPositions(posses);
+            var aslist = posses.ToList();
+            aslist.RemoveAt(1);
+            a.SetPositions(aslist.ToArray());
+        }
     }
 
     protected override void FixedUpdate()
@@ -42,12 +51,16 @@ public class MovingFlipper : Flipper
 
         var left = point1.position;
         var right = point2.position;
-        var bottom = point3.position;
-        var mid = (left + right) / 2;
+        Vector3? bottom = point3 != null ? point3.position : null;
+        Vector3? mid = point4 != null ? point4.position : null;//
        
         bool leftright = Vector3.Distance((left - (Vector3)rigidbody.position).normalized, (left - right).normalized) < 0.05f;
-        bool middown = Vector3.Distance((bottom - (Vector3)rigidbody.position).normalized, (bottom - mid).normalized) < 0.05f;
-        if (Vector3.Distance(rigidbody.position, mid) < 0.1f || Vector3.Distance(rigidbody.position, bottom) < 0.1f)
+
+        bool middown = false;
+        if (bottom != null && mid != null)
+            middown = Vector3.Distance(((Vector3)bottom - (Vector3)rigidbody.position).normalized, ((Vector3)bottom - (Vector3)mid).normalized) < 0.05f;
+        
+        if (mid != null && bottom != null && (Vector3.Distance(rigidbody.position, (Vector3)mid) < 0.1f || Vector3.Distance(rigidbody.position, (Vector3)bottom) < 0.1f))
             middown = true;
         if (Vector3.Distance(rigidbody.position, left) < 0.1f || Vector3.Distance(rigidbody.position, right) < 0.1f)
             leftright = true;
@@ -56,12 +69,12 @@ public class MovingFlipper : Flipper
         if (leftright)
             options.AddRange(new[] { left, right });
         if (middown)
-            options.AddRange(new[] { mid, bottom });
+            options.AddRange(new[] { (Vector3)mid, (Vector3)bottom });
 
         if (options.Count == 0)
             return;
         
-        // if (AltInputKeyboard is { } b)
+        // if (AltInputKeyboard is { } b)//
         // {
         //    if (Time.deltaTime * movementSpeed * 2 is { } step &&
         //        step > Vector3.Distance(rigidbody.position, (b ? chosenPoint1 : chosenPoint2)))
