d9ca27dc3cf5eca7e2ad299ab313b6c1351b07b5
boi-one <91535051+boi-one@users.noreply.github.com>
2025-05-21 21:51:27 +0200

    sound effects (todo change volume insettings)

--- Assets/Scripts/Ball/Ball.cs
+++ Assets/Scripts/Ball/Ball.cs
@@ -8,8 +8,10 @@ public class Ball : MonoBehaviour
     public UnityEvent Enable;
     public UnityEvent Disable;
 
+    public int hitEffectIndex = 2;
+
     float teleportWidth;
-    
+
     [HideInInspector]
     public Rigidbody2D rigidBody;
     SpriteRenderer spriteRenderer;
@@ -19,7 +21,7 @@ public class Ball : MonoBehaviour
 
     [SerializeField]
     float damagableVelocity;
-    
+
     [SerializeField]
     float maxDurability = 35f;
     public float getMaxDurability
@@ -45,11 +47,12 @@ public class Ball : MonoBehaviour
                 }
             }
         }
-    } float? _durability;
+    }
+    float? _durability;
 
     void DisableBall()
     {
-        foreach(var p in Powerup.allPowerups)
+        foreach (var p in Powerup.allPowerups)
         {
             p.OnCancel.Invoke();
         }
@@ -63,10 +66,10 @@ public class Ball : MonoBehaviour
     bool ready = true;
     float time;
     [SerializeField] float moveTime;
-    
+
     void EnableBall()
     {
-        foreach(var p in Powerup.allPowerups)
+        foreach (var p in Powerup.allPowerups)
             p.SetPowerup();
 
         GetComponent<ParticleSystem>().Stop();
@@ -81,7 +84,7 @@ public class Ball : MonoBehaviour
 
     float beginTime = 5f;
     bool firstTime = true;
-    
+
     IEnumerator WaitForDrop(float waitTime = 3f)
     {
         ready = true;
@@ -92,9 +95,9 @@ public class Ball : MonoBehaviour
             yield return new WaitForSeconds(beginTime);
             firstTime = false;
         }
-        
+
         yield return new WaitForSeconds(waitTime);
-        
+
         rigidBody.simulated = true;
         ready = false;
     }
@@ -102,7 +105,7 @@ public class Ball : MonoBehaviour
     void Awake()
     {
         reference = this;
-        teleportWidth = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0)).x  + 1;
+        teleportWidth = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0)).x + 1;
         startPosition = transform.position;
         rigidBody = GetComponent<Rigidbody2D>();
         phases = GetComponent<Phases>();
@@ -126,7 +129,7 @@ public class Ball : MonoBehaviour
             transform.position = Vector3.Lerp(startPosition + new Vector3(startPosition.x < 0 ? -2 : 2, 0, 0), startPosition, time / moveTime);
         }
         else time = 0;
-        
+
         TeleportSideScreen(teleportWidth);
 
         if (transform.position.y < -5 + LevelSystem.instance.amountOfLevels * LevelSystem.instance.gapBetweenLevels)
@@ -135,14 +138,15 @@ public class Ball : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
+        SFXManager.reference.Play(0);
+
         if (impactVelocity.magnitude > damagableVelocity)
         {
+            SFXManager.reference.Play(hitEffectIndex);
             Durability -= impactVelocity.magnitude;
         }
     }
 
-    Vector3 teleportLocation;
-    
     void TeleportSideScreen(float x = 2f)
     {
         var pos = transform.position;
---
+++ Assets/Scripts/SoundEffectManager/SFXManager.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXManager : MonoBehaviour
+{
+    public static SFXManager reference;
+    AudioSource source;
+    [SerializeField] //order is the same as in the sound effect folder (excluding (dis)connection, lose and win sfx)
+    List<AudioClip> soundEffects = new List<AudioClip>();
+
+    void Awake()
+    {
+        reference = this;
+        source = GetComponent<AudioSource>();
+        Ball.reference.Disable.AddListener(() => Play(4));
+    }
+
+    public void Play(int audioIndex)
+    {
+        source.PlayOneShot(soundEffects[audioIndex]);
+    }
+
+    public void SetVolume(float volume)
+    {
+        source.volume = Mathf.Clamp01(volume);
+    }
+}
