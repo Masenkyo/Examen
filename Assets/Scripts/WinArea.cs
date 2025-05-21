7e1d5bd8790c23d4c609975570b5638485661c95
boi-one <91535051+boi-one@users.noreply.github.com>
2025-05-21 21:58:52 +0200

    sound effects (todo change volume insettings)

--- Assets/Scripts/WinArea.cs
+++ Assets/Scripts/WinArea.cs
@@ -6,6 +6,7 @@ public class WinArea : MonoBehaviour
     public static Action OnWin = () => { };
     void OnCollisionEnter2D(Collision2D other)
     {
+        Camera.main.GetComponent<AudioSource>().mute = true;
         if (other.gameObject.TryGetComponent<Ball>(out _))
             OnWin();
     }
