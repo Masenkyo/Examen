7da336134a7289c3f0dc33bac02f7d46d0f7a3d9
Boi <91535051+boi-one@users.noreply.github.com>
2025-05-23 10:10:23 +0200

    fixed some code convention violations

--- Assets/Scripts/checkpoint/checkpoint.cs
+++ Assets/Scripts/checkpoint/checkpoint.cs
@@ -19,7 +19,7 @@ public class checkpoint : MonoBehaviour
         if (!checkPoint) return;
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.TryGetComponent<Ball>(out var ball))
         {
